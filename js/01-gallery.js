33359ea3a65d35d353bf2ab8f373543d0e0c4927
Vira Voronova <vera.voronova@hotmail.com>
2022-07-23 15:27:40 +0200

    first task done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,83 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 /* 
 Создай галерею с возможностью клика по её элементам и 
 просмотра полноразмерного изображения в модальном окне. 
 */
 
-const gallery = document.querySelector('.gallery');
+const galleryRef = document.querySelector('.gallery');
 
+function createGalleryMarkup(galleryItems) {
+
+  const galleryMarkup = galleryItems
+    .map(item => {
+
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+          alt="${item.description}"
+          />
+        </a>
+      </div>`
+    })
+  .join('')
+
+  return galleryMarkup;
+}
+
+galleryRef.insertAdjacentHTML('beforeend',createGalleryMarkup(galleryItems))
+
+
+galleryRef.addEventListener('click', onGalleryContainerClick);
+
+
+function onGalleryContainerClick(event) {
+  
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return
+  };
+
+  openLightboxImage(event.target.dataset.source);
+  
+}
+  
+
+function openLightboxImage(imageSourse) {
+
+  const element = basicLightbox.create(`
+		<img width="1400" height="900" src="${imageSourse}">
+	`, {
+    onShow: () => {
+      document.addEventListener('keydown', onEcsKeyDown);
+    },
+    onClose: () => {
+      document.removeEventListener('keydown', onEcsKeyDown);
+    }
+  })
+
+  function onEcsKeyDown(event) {
+    if (event.code === "Escape") {
+      element.close()
+    }
+  }
+
+  element.show()
+}
+
+
+
+
+// *создание галлеери через createElement
+/* 
 function createGallery(galleryItems) {
 
   const galleryElements = galleryItems
@@ -30,6 +98,7 @@ function createGallery(galleryItems) {
     })
 
     gallery.append(...galleryElements);
-}
+}    
 
 createGallery(galleryItems)
+*/
